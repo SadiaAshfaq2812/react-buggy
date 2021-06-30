4fbc5158f38013cabb761bf9a3e4b6cc5e57dd3b
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 18:01:08 +0500

    run bash file scripts

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -1,1 +1,4 @@
-./dependency-check/bin/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
+echo "Hello there"
+echo "My name is $(whoami)"
+echo "Nice to meet you"
+# /dependency-check/bin/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
