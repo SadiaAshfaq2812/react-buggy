54069ea45bfdcf1dcae5f305abecdc8c11b80126
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 19:56:07 +0500

    modified dependency-check-script.sh file

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -1,11 +1,6 @@
-echo "Hello there"
-echo "My name is $(whoami)"
-echo "Nice to meet you"
 
-# ./dependency-check.sh -h
-# ./dependency-check.sh --out . --scan ./
 cd ./dependency-check/bin
 chmod +x ./dependency-check.sh
-pwd
-# ls -F
 ./dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities
+cd ..
+cd ..
