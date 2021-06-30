5b33475afa0d66aa22b5552171302bb4dbe56fe2
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 18:44:01 +0500

    modification in current directory

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -1,7 +1,9 @@
 echo "Hello there"
 echo "My name is $(whoami)"
 echo "Nice to meet you"
-./dependency-check.sh -h
-./dependency-check.sh --out . --scan ./
 
-# ./dependency-check/bin/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
+pwd
+# ./dependency-check.sh -h
+# ./dependency-check.sh --out . --scan ./
+
+./dependency-check/bin/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
