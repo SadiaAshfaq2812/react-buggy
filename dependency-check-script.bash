f95086b458249ecb3fd705f2821190fe2caaf1e4
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 19:48:16 +0500

    added permission

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -5,6 +5,7 @@ echo "Nice to meet you"
 # ./dependency-check.sh -h
 # ./dependency-check.sh --out . --scan ./
 cd ./dependency-check/bin
+chmod +x ./dependency-check.sh
 pwd
 # ls -F
 ./dependency-check.sh --project react-project --scan ../../../react-buggy --out ModuleVulnerabilities
