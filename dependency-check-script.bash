6f6727cf858074f7c88c32ba9b88a408b9e0bd27
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 18:19:19 +0500

    removed $ sign from bash file

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -1,7 +1,7 @@
 echo "Hello there"
 echo "My name is $(whoami)"
 echo "Nice to meet you"
-$ ./dependency-check/bin/dependency-check.sh -h
-$ ./dependency-check/bin/dependency-check.sh --out . --scan ./
+./dependency-check/bin/dependency-check.sh -h
+./dependency-check/bin/dependency-check.sh --out . --scan ./
 
 # ./dependency-check/bin/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
