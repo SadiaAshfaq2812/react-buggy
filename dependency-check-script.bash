b1699c16aab32f4fae601f797f6b33d09fc2b5a6
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 18:49:21 +0500

    changed path

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -6,4 +6,4 @@ pwd
 # ./dependency-check.sh -h
 # ./dependency-check.sh --out . --scan ./
 
-/workspace:/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
+$(pwd):/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
