9fc774a8480cd7280ed311a26b6935b08b0aaa87
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 18:56:32 +0500

    changed directory

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -4,7 +4,7 @@ echo "Nice to meet you"
 
 # ./dependency-check.sh -h
 # ./dependency-check.sh --out . --scan ./
-cd ./react-buggy/dependency-check/bin
+cd ./dependency-check/bin
 pwd
 ls -F
 # $(pwd):/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
