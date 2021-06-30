6144d41a8310036b95f068bbbb8108ff530826d4
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-06-30 18:55:24 +0500

    chnaged command

--- dependency-check-script.bash
+++ dependency-check-script.bash
@@ -6,4 +6,5 @@ echo "Nice to meet you"
 # ./dependency-check.sh --out . --scan ./
 cd ./react-buggy/dependency-check/bin
 pwd
+ls -F
 # $(pwd):/dependency-check.sh --project react-project --scan gcr.io/thesisproject1859/react --out ModuleVulnerabilities
