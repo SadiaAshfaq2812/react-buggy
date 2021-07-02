87252bd9ab655b86674e1c56d7ff7ac16720b33b
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-02 19:06:06 +0500

    code repo

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -1,6 +1,6 @@
 
 cd ./dependency-check/bin
 chmod +x ./dependency-check.sh
-./dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities
+./dependency-check.sh --project react-project --scan ../../../react-buggy --out ModuleVulnerabilities
 cd ..
 cd ..
