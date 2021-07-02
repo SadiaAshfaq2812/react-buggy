9c90989b9bcab0f3bfaddf7a37c9b168e9016059
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-02 19:25:15 +0500

    modified

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -3,7 +3,7 @@
 cd workspace
 pwd
 ls -F
-chmod +x ./dependency-check/bin/dependency-check.sh
-./dependency-check/bin/dependency-check.sh --project react-project --scan ./react-buggy --out ModuleVulnerabilities
+# chmod +x ./dependency-check/bin/dependency-check.sh
+# ./dependency-check/bin/dependency-check.sh --project react-project --scan ./react-buggy --out ModuleVulnerabilities
 # cd ..
 # cd ..
