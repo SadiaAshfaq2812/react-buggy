dc03271bedea7b5245e752d8effce13947cedb6d
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-02 19:10:17 +0500

    path modified

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -1,8 +1,8 @@
 
-cd ./dependency-check/bin
+# cd ./dependency-check/bin
 pwd
 ls -F
 chmod +x ./dependency-check.sh
-./dependency-check.sh --project react-project --scan ../../../react-buggy --out ModuleVulnerabilities
+./dependency-check/bin/dependency-check.sh --project react-project --scan ./react-buggy --out ModuleVulnerabilities
 cd ..
 cd ..
