b2cf1e97bcad52f05841df007b77d408460637dd
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-02 19:10:51 +0500

    change

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -2,7 +2,7 @@
 # cd ./dependency-check/bin
 pwd
 ls -F
-chmod +x ./dependency-check.sh
+chmod +x ./dependency-check/bin/dependency-check.sh
 ./dependency-check/bin/dependency-check.sh --project react-project --scan ./react-buggy --out ModuleVulnerabilities
-cd ..
-cd ..
+# cd ..
+# cd ..
