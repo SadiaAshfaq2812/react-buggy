b71c3612aadadb14bfca001546bf8f7af2accd0f
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-02 19:57:48 +0500

    removed commented code

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -1,9 +1,4 @@
 
-# cd ./dependency-check/bin
-# cd workspace
-# pwd
-ls -F
 chmod +x ./dependency-check/bin/dependency-check.sh
+
 ./dependency-check/bin/dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities
-# cd ..
-# cd ..
