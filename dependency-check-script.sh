3d5dec4eae035fed28cd42331df1fa1e4f204c48
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-22 02:53:39 +0500

    added runtime param

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -1,11 +1,11 @@
 ls -la
 
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
+# wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
+# rm packages-microsoft-prod.deb
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y dotnet-sdk-5.0
+# sudo apt-get update
+# sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y dotnet-sdk-5.0
 
 chmod +x ./dependency-check/bin/dependency-check.sh
 
