6afeab2f0f9a05b1fb1c28b56e8158024a95a594
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-23 16:15:38 +0500

    changed order

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -9,4 +9,4 @@ ls -la
 
 chmod +x ./dependency-check/bin/dependency-check.sh
 
-./dependency-check/bin/dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities --disableNodeJS true --disableAssembly true
+./dependency-check/bin/dependency-check.sh --disableNodeJS true --disableAssembly true --project react-project --scan ./ --out ModuleVulnerabilities
