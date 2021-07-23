fa2f937d11e8582ac3c9beae45d668f8640552d2
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-23 06:52:30 +0500

    reverted dependency-check file changes

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -9,5 +9,4 @@ ls -la
 
 chmod +x ./dependency-check/bin/dependency-check.sh
 
-./dependency-check/bin/dependency-check.sh --project react-project --scan ./ --out .
-# ./dependency-check/bin/dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities
+./dependency-check/bin/dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities
