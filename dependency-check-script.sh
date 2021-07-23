3ce1704a3d3ba9e48ec4ed87ed27d4ae945dd6a5
sadia19321 <sadia.ashfaq@visionetsystems.com>
2021-07-23 15:50:58 +0500

    added disableNodeJs and disableAssembly param for dependency-check

--- dependency-check-script.sh
+++ dependency-check-script.sh
@@ -9,4 +9,4 @@ ls -la
 
 chmod +x ./dependency-check/bin/dependency-check.sh
 
-./dependency-check/bin/dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities
+./dependency-check/bin/dependency-check.sh --project react-project --scan ./ --out ModuleVulnerabilities --disableNodeJS true --disableAssembly true
